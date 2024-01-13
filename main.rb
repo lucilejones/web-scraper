a8a1c338c0dc4caace1c884993952a01b83f132c
lucilejones <99698725+lucilejones@users.noreply.github.com>
2024-01-13 22:57:48 +0000

    Also working with httparty

--- main.rb
+++ main.rb
@@ -1,16 +1,20 @@
 require "nokogiri"
-# require "httparty"
-require "open-uri"
+require "httparty"
+# require "open-uri"
+
 # keep getting an error saying it can't load the httparty file
-# require_relative '.bundle/ruby/3.1.2/gems/httparty-0.21.0/lib/httparty.rb'
+# got suggestions from 
+# https://replit.com/talk/announcements/Ruby-Packages-via-RubyGems/11349?order=new
+
+# Works when I use: bundle exec ruby filname.rb
 
 
 
 class API
   def self.get_films_by_year(year)
     url = "https://en.wikipedia.org/wiki/#{year}_in_film"
-    # unparsed_page = HTTParty.get(url)
-    unparsed_page = URI.open(url)
+    unparsed_page = HTTParty.get(url)
+    # unparsed_page = URI.open(url)
     # parsed_page = Nokogiri::HTML(unparsed_page.body)
     parsed_page = Nokogiri::HTML(unparsed_page)
     
